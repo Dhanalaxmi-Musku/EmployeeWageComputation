58ddb120b02e3885dde5c093c5dadf41dc693cc9
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-02 16:20:02 +0530

    Fourth commit Added:part time wage and used switch

--- src/Employee.java
+++ src/Employee.java
@@ -3,27 +3,46 @@ public class Employee {
     String name;
     int id;
     boolean isPresent;
+    int workingHours;
     Employee(String name, int id) {
         this.name = name;
         this.id = id;
     }
-	public void checkAttendance() {
+    public void checkAttendance() {
         Random random = new Random();
-        isPresent = random.nextBoolean();
+        int attendanceType = random.nextInt(3);
         
-        if (isPresent) {
-            System.out.println(name + " is present today.");
-        } else {
-            System.out.println(name + " is absent today.");
+        switch (attendanceType) {
+            case 0: 
+                handleAttendance(false, 0);
+                break;
+            case 1: 
+                handleAttendance(true, EmployeeWageComputation.FULL_DAY_HOURS);
+                break;
+            case 2: 
+                handleAttendance(true, EmployeeWageComputation.PART_TIME_HOURS);
+                break;
         }
     }
-	public void calculateDailyWage() {
+    private void handleAttendance(boolean present, int hours) {
+        isPresent = present;
+        workingHours = hours;
+        
+        if (!present) {
+            System.out.println(name + " is absent today.");
+        } else {
+            String workType = (hours == EmployeeWageComputation.FULL_DAY_HOURS) ? 
+                              "full day" : "part-time";
+            System.out.println(name + " is working " + workType + ".");
+        }
+    }
+    public void calculateDailyWage() {
         if (isPresent) {
-            int dailyWage = EmployeeWageComputation.WAGE_PER_HOUR * 
-                            EmployeeWageComputation.FULL_DAY_HOURS;
+            int dailyWage = EmployeeWageComputation.WAGE_PER_HOUR * workingHours;
             System.out.println("Daily Wage: " + dailyWage);
         } else {
             System.out.println("No wage as employee is absent.");
         }
     }
+	
 }
--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class EmployeeWageComputation {
 	public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 4;
     public static void main(String[] args) {
     	System.out.println("Welcome to Employee Wage Computation");
         Scanner scan=new Scanner(System.in);
